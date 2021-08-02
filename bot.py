e10ea65f1d707b34ec1bf23813b955045fbdfede
kolyakot33 <kolyakot33@gmail.com>
2021-08-02 12:13:08 +0300

    buy

--- bot.py
+++ bot.py
@@ -4,10 +4,14 @@ from traceback import print_exc, format_exc
 import discord
 from signal import signal, SIGINT, SIGTERM
 from discord.ext import tasks
+import pymysql
 
-client = discord.Client()
+client = discord.Client(intents=discord.Intents.all())
 start_time = time()
 state = 1
+
+con = pymysql.connect(host="5.252.194.76", user="u24_Gy3siZPRMr", password="!v9+4cr!bQa2Wwo=y51zeu1+",
+                      database="s24_main")
 
 
 @tasks.loop(seconds=30)
@@ -26,11 +30,11 @@ async def on_ready():
 
 
 @client.event
-async def on_error(err):
+async def on_error(*args):
     try:
         await client.get_user(632511458537898016).send(format_exc())
-    except Exception:
-        print("Fatal Error!")
+    except Exception as exc:
+        print("Fatal Error!", exc)
     finally:
         print_exc()
 
@@ -39,7 +43,8 @@ async def on_error(err):
 async def on_message(message: discord.Message):
     if message.author == client.user or state == 0:
         return
-    elif ("@everyone" in message.content.lower() or "@here" in message.content.lower()) and not message.author.guild_permissions.administrator:
+    elif (
+            "@everyone" in message.content.lower() or "@here" in message.content.lower()) and not message.author.guild_permissions.administrator:
         await message.channel.send("Какой тебе ещё пинг? Бан хочешь?")
         await message.delete()
         return
@@ -68,23 +73,38 @@ async def on_message(message: discord.Message):
             embed.set_footer(text="Homka",
                              icon_url="https://cdn.discordapp.com/attachments/843588784126033943/870815009293361173/Screenshot_74.png")
         await message.channel.send(embed=embed, content=usr)
-    elif message.content.lower().startswith("!makeann"):
+    elif message.content.lower().startswith("!makeann"): # and message.channel.id == 858986069553840138:
         try:
             type, resource, price = message.content[9:].split(sep=";")
         except ValueError:
             await message.reply("Ошибка в синтаксисе команды")
         embed = discord.Embed(title="Объявление", colour=int("2f3136", base=16))
-        embed.add_field(name="Тип", value="Продажа" if type=="1" else "Покупка")
+        embed.add_field(name="Тип", value="Продажа" if type == "1" else "Покупка")
         embed.add_field(name="Ресурсы", value=resource)
         embed.add_field(name="Цена", value=f"{price} <:lar:858797748924448788>")
         embed.set_footer(icon_url=message.author.avatar_url, text=message.author.name)
-        await message.channel.send(embed=embed)
+        msg = await message.channel.send(embed=embed)
+        cur = con.cursor()
+        cur.execute(f"INSERT INTO ann (message, user) VALUES ({msg.id}, {message.author.id})")
+        cur.close()
+        con.commit()
         await message.delete()
         return
     elif message.channel.id == 858986069553840138:
         await message.delete()
-
-
+        return
+    elif message.content.lower().startswith("!buy"):
+        iD, comment = message.content[5:].split(sep=';')
+        cur = con.cursor()
+        cur.execute(f"SELECT user, message FROM ann WHERE id={iD}")
+        res = cur.fetchone()
+        cur.close()
+        print(res)
+        user = client.get_user(int(res[0]))
+        embed = discord.Embed(title="Покупка", description=f"{user.mention}, у вас хотят купить ресурсы по этому айди: [{iD}]({client.get_channel(858986069553840138).get_partial_message(int(res[1])).jump_url})", colour=int("6cc789", base=16))
+        embed.add_field(name="Комментарий покупателя", value=comment)
+        embed.set_footer(icon_url=message.author.avatar_url, text=message.author.name)
+        await client.get_channel(871026946581073941).send(embed=embed, content=user.mention)
 
 def bot_stop(*args):
     global state
@@ -95,6 +115,7 @@ def bot_stop(*args):
     refresh_status.stop()
     for task in asyncio.all_tasks():
         task.cancel()
+    con.close()
     asyncio.get_running_loop().stop()
     print("Stop completed!")
 

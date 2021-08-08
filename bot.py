6cc4b6999c9d3e2ad17085b4c4b02296fbe39338
Kolyakot33 <73388223+Kolyakot33@users.noreply.github.com>
2021-08-08 20:24:42 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -148,7 +148,7 @@ async def remove_ann(ctx : ComponentContext):
     if int(cur.fetchone()[0]) == ctx.author_id:
         await ctx.origin_message.delete()
     else:
-        ctx.send("Ошибка: вы не создатель объявления", hidden=True', delete_after=10.0)
+        ctx.send("Ошибка: вы не создатель объявления", hidden=True, delete_after=10.0)
     cur.close()
     con.close()
 
